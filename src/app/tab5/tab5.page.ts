c1cc340d0e0e669d3720c8a457089a7214dd085e
jcmorello <jcmorello@ontinet.com>
2020-01-21 12:10:16 +0100

    add minlength for email

--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -17,7 +17,8 @@ export class Tab5Page implements OnInit {
       '',
       [
         Validators.required,
-        Validators.pattern('^[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$')
+        Validators.pattern('^[a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$'),
+        Validators.minLength(5)
       ]
     ],
     phone: [
@@ -42,11 +43,12 @@ export class Tab5Page implements OnInit {
   public errorMessages = {
     name: [
       { type: 'required', message: 'Name is required' },
-      { type: 'maxlength', message: 'Name cant be longer than 100 chars' },
+      { type: 'maxlength', message: 'Name cant be longer than 100 characters' },
     ],
     email: [
       { type: 'required', message: 'Email is required' },
       { type: 'pattern', message: 'Please enter a valid email address' },
+      { type: 'minlength', message: 'email cant be shorter than 5 characters' },
     ],
     phone: [
       { type: 'required', message: 'Phone is required' },
@@ -54,19 +56,19 @@ export class Tab5Page implements OnInit {
     ],
     street: [
       { type: 'required', message: 'Street is required' },
-      { type: 'maxlength', message: 'Street cant be longer than 100 chars' },
+      { type: 'maxlength', message: 'Street cant be longer than 100 characters' },
     ],
     city: [
       { type: 'required', message: 'City is required' },
-      { type: 'maxlength', message: 'City cant be longer than 100 chars' },
+      { type: 'maxlength', message: 'City cant be longer than 100 characters' },
     ],
     state: [
       { type: 'required', message: 'State is required' },
-      { type: 'maxlength', message: 'State cant be longer than 100 chars' },
+      { type: 'maxlength', message: 'State cant be longer than 100 characters' },
     ],
     zip: [
       { type: 'required', message: 'Zip is required' },
-      { type: 'maxlength', message: 'Zip cant be longer than 100 chars' },
+      { type: 'pattern', message: 'Please enter a valid phone number' },
     ]
   }
 
